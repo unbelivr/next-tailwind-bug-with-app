a25013c26edda32db130bd0cb50ac5519e231ebc
Maxim Svetlakov <maxim@ebookapplications.com>
2023-02-21 20:42:22 +0500

    Update to latest canary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
   return (
     <main className="text-3xl bg-black flex flex-col justify-between items-center min-h-screen">
       Change this line, save and see how black bg flashes
-      saddasd
     </main>
   )
 }
