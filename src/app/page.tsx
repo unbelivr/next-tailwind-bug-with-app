85894021ed799ee61489bd7e7e61ef6e52450bfb
Maxim Svetlakov <maxim@ebookapplications.com>
2023-02-21 20:34:51 +0500

    Init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main className="text-3xl bg-black flex flex-col justify-between items-center min-h-screen">
+      Change this line, save and see how black bg flashes
+      saddasd
+    </main>
+  )
+}
